d44bf1f5c810e2dd942daf2c6c6eedcd21c309f6
degurii <sansam709@gmail.com>
2020-10-13 00:14:54 +0900

    CH17.4까지 완료

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Coutner from './components/Counter';
+import Todos from './components/Todos';
 
 const App = () => {
   return (
     <div>
       <Coutner number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 };
