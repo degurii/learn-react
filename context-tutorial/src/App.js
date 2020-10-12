79cbe3ed7437b68d62fd7a6266c4d361976123a2
degurii <sansam709@gmail.com>
2020-10-12 21:32:21 +0900

    Ch15. context api tutorial 끝

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import SelectColors from './components/SelectColors';
 import { ColorProvider } from './contexts/color';
 
 const App = () => {
   return (
     <ColorProvider>
-      <div>
+      <div style={{ marginLeft: '10px' }}>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
