c72941713fe3bb5a5321e0942ebc35803e1c7ca0
degurii <sansam709@gmail.com>
2020-10-12 23:24:48 +0900

    init

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Coutner from './components/Counter';
+
+const App = () => {
+  return (
+    <div>
+      <Coutner number={0} />
+    </div>
+  );
+};
+
+export default App;
